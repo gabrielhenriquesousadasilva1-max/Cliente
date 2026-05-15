f65392774304bac38ab1de1e0c26b0e6bce75f33
gabrielhenriquesousadasilva1-max <gabrielhenriquesousadasilva1@gmail.com>
2026-05-15 08:25:24 -0300

    Cadastro de Cliente

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,65 +1,69 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
-
 from .forms import ClienteForm, UsuarioForm
 from .models import Cliente
+# Create your views here.
 
 @login_required
 def novo_cliente(request):
-    clientes = Cliente.objects.all() # SELECT * FROM clientes
+    clientes = Cliente.objects.all()
     template_name = 'novo_cliente.html'
     context = {}
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('novo_cliente')  # Redireciona para a mesma página após salvar
+            return redirect('novo_cliente')
         else:
-            return HttpResponse('<h1>Deu erro no teu formulário<h1>')
-        
+            return HttpResponse('<h1>,Erro no formulário<h1>')
+    
     form = ClienteForm()
     context['form'] = form
     context['clientes'] = clientes
-
-    return render(request, template_name, context)
+    return render (request, template_name, context)
 
 @login_required
 def atualizar_cliente(request, id):
     try:
-        cliente = Cliente.objects.get(id=id)
+        cliente = Cliente.objects.get(pk=id)
     except Cliente.DoesNotExist:
-        return HttpResponse('<h1>Cliente não encontrado')
-
+        return HttpResponse('<h1>Cliente não encontrado</h1>')
     
     if request.method == 'POST':
         form = ClienteForm(request.POST, instance=cliente)
         if form.is_valid():
             form.save()
-            return redirect('novo_cliente')  
+            return redirect('novo_cliente')
         else:
-         return HttpResponse('<h1>Erro na atualização do cliente<h1>')
+            print(form.errors)
+            return HttpResponse('<h1>Erro na atualização do cliente</h1>')
+        
+
 
     form = ClienteForm(instance=cliente)
     template_name = 'novo_cliente.html'
-    cliente = Cliente.objects.all()
+    clientes = Cliente.objects.all()
     context = {
-    'form': form,
-    'clientes': cliente
+        'form': form,
+        'clientes': clientes
+        
     }
     return render(request, template_name, context)
 
 @login_required
 def excluir_cliente(request, id):
-    try: 
-     cliente = Cliente.objects.get(id=id)
+    try:
+        cliente = Cliente.objects.get(id=id)
+        cliente.delete()
     except Cliente.DoesNotExist:
-        return HttpResponse('<h1>Erro ao excluir.Cliente não encontrado<h1>')
+        return HttpResponse('<h1>Erro ao excluir o cliente. Não encontrado</h1>')
     return redirect('novo_cliente')
+
 
 def login_usuario(request):
     template_name = 'login.html'
@@ -69,36 +73,40 @@ def login_usuario(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             usuario = authenticate(username=username, password=password)
+            
             if usuario is not None:
                 login(request, usuario)
                 return redirect('novo_cliente')
         else:
-            return HttpResponse(request, "Usuário ou senha inválidos")
+            return HttpResponse(request, "Usuário ou senha inválidos.")
     else:
-            form = AuthenticationForm()
-            context = {
-                'form': form
-            }
+        form = AuthenticationForm()
+    
+    context = {'form': form}
+        
     return render(request, template_name, context)
+
+
 
 
 def novo_usuario(request):
     template_name = 'novo_usuario.html'
-    if request.method =='POST':
+    if request.method == 'POST':
         form = UsuarioForm(request.POST)
         if form.is_valid():
-          f = form.save(commit=False)
-          f.set_password(f.password)
-          f.save()
-          return redirect('login_usuario')
+            f = form.save(commit=False)
+            f.set_password(f.password)
+            f.save()
+            return redirect('login_usuario')
         else:
-          return HttpResponse('Erro ao criar usuário')
-    else: 
+            return HttpResponse('Erro ao criar o usuário')
+    else:
         form = UsuarioForm()
     context = {'form': form}
     return render(request, template_name, context)
 
+
 @login_required
-def sair (request):
-    logout (request)
+def sair(request):
+    logout(request)
     return redirect('login_usuario')
